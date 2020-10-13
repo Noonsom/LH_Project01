ecf0e89cadda764670c0330b98ce77a125235c60
Noonsom <46413821+Noonsom@users.noreply.github.com>
2020-10-13 10:34:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>V월드2</title>
+<title>V월드3</title>
 
 <style>
  html, body {margin: 0;padding: 0;height: 100%;}
